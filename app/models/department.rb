59de04b6d1f90c3d179d3f5daae55e0dc56e2835
shanpu <cheztomo513@gmail.com>
2018-06-24 14:43:23 +0900

    リレーションを追加

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,7 +1,6 @@
-class Department 
+class Department
   include Neo4j::ActiveNode
   property :name, type: String
 
-
-
+  has_many :in, :universities, type: :has, model_class: :University, dependent: :destroy_orphans
 end
--- app/models/university.rb
+++ app/models/university.rb
@@ -1,7 +1,6 @@
-class University 
+class University
   include Neo4j::ActiveNode
   property :name, type: String
 
-
-
+  has_many :out, :departments, type: :has, model_class: :Department, dependent: :destroy_orphans
 end
